9c8332dd6ac79f3c450bf1fd4ab74c86c1b9dd24
Caleb Durden <calebdurden@gmail.com>
2022-04-15 03:41:31 -0400

    commit tree 02222ab3487e87c8057bbb2ea93e4f357033cc11

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const newCats = cats.slice();
+    newCats.push("Broom");
+    return newCats;
+}
+
+function prependCat(name) {
+    const coolCats = cats.slice();
+    coolCats.unshift("Arnold");
+    return coolCats;
+}
+
+function removeLastCat() {
+    const noLastCat = cats.slice();
+    noLastCat.pop();
+    return noLastCat
+}
+
+function removeFirstCat() {
+    const noFirstCat = cats.slice();
+    noFirstCat.shift();
+    return noFirstCat;
+}
